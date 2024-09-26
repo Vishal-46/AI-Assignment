d9a904ae838db24d146f1ed931516bd46a7e827b
Vishal <vishalmurugan006@gmail.com>
2024-09-26 19:36:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var sidenavbar=document.querySelector(".side-navbar")
+
+
+
+function shownavbar(){
+    sidenavbar.style.display="block"
+}
+function closenavbar(){
+    sidenavbar.style.display="none"
+}
